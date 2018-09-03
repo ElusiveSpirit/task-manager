b8e92b8344b425e91b6e49f5802e3ea0394324f2
Konstantin <2447785@gmail.com>
2018-09-03 10:37:01 +0300

    Remove scripts from profile.vue

--- pages/profile.vue
+++ pages/profile.vue
@@ -2,32 +2,11 @@
   <v-layout align-center justify-center>
     <v-flex xs12 sm10 md8>
       User email: {{ $store.state.user.email }}
-      <v-btn
-          @click="go"
-      >
-        GO
-      </v-btn>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
-  import User from '~/models/User'
-  // import Email from '~/models/Email'
-
   export default {
-    async mounted () {
-      // const user = await User.create({
-      //   id: 10,
-      //   username: 'TESTETSTETS'
-      // })
-      // console.log(user)
-    },
-    methods: {
-      async go () {
-        console.log(await User.fetch({ username: '42.zip' }))
-        // console.log(await Email.fetch())
-      }
-    }
   }
 </script>
